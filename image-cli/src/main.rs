54ed9bd5da228b8305b90b8f7c63db82cc778b5b
SONG MING <songm@neusoft.com>
2025-01-15 07:38:13 +0800

    fix error.

--- image-cli/src/main.rs
+++ image-cli/src/main.rs
@@ -1,17 +1,19 @@
-
 extern crate structopt;
 mod imagix;
 
-use ::imagix::error::ImagixError;
-use ::imagix::resize::{process_resize_request, Mode, SizeOption};
-use ::imagix::stats::get_stats;
+use imagix::error::ImagixError;
+use imagix::resize::{process_resize_request, Mode, SizeOption};
+use imagix::stats::get_stats;
 
 use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "resize", about = "This is a tool for resizing images.",
-help = "Specify subcommand resize or stats. For help type image-cli resize --help or image-cli stats --help")]
+#[structopt(
+    name = "resize",
+    about = "This is a tool for resizing images.",
+    help = "Specify subcommand resize or stats. For help type image-cli resize --help or image-cli stats --help"
+)]
 enum Commandline {
     #[structopt(help = "Specify size(small/medium/large), mode(single/all) and src folder")]
     Resize {
@@ -32,28 +34,28 @@ enum Commandline {
 fn main() {
     let args = Commandline::from_args();
     match args {
-        Commandline::Resize { size, mode, mut srcfolder } => {
-            match process_resize_request(size, mode, &mut srcfolder) {
-                Ok(_) => println!("Images resized successfully"),
-                Err(e) => match e {
-                    ImagixError::FileIOError(e) => eprintln!("File operator failed: {}", e),
-                    ImagixError::UserInputError(e) => eprintln!("User input error: {}", e),
-                    ImagixError::ImageResizingError(e) => eprintln!("Image resize error: {}", e),
+        Commandline::Resize {
+            size,
+            mode,
+            mut srcfolder,
+        } => match process_resize_request(size, mode, &mut srcfolder) {
+            Ok(_) => println!("Images resized successfully"),
+            Err(e) => match e {
+                ImagixError::FileIOError(e) => eprintln!("File operator failed: {}", e),
+                ImagixError::UserInputError(e) => eprintln!("User input error: {}", e),
+                ImagixError::ImageResizingError(e) => eprintln!("Image resize error: {}", e),
 
-                    _ => println!("Format error: {}", e),
-                }            
-            }
-        }
-        Commandline::Stats { srcfolder } => {
-            match get_stats(srcfolder) {
-                Ok(_) => println!("Stats generated successfully"),
-                Err(e) => match e {
-                    ImagixError::FileIOError(e) => eprintln!("File operator failed: {}", e),
-                    ImagixError::UserInputError(e) => eprintln!("User input error: {}", e),
+                _ => println!("Format error: {}", e),
+            },
+        },
+        Commandline::Stats { srcfolder } => match get_stats(srcfolder) {
+            Ok(_) => println!("Stats generated successfully"),
+            Err(e) => match e {
+                ImagixError::FileIOError(e) => eprintln!("File operator failed: {}", e),
+                ImagixError::UserInputError(e) => eprintln!("User input error: {}", e),
 
-                    _ => println!("Format error: {}", e),
-                }
-            }
-        }
+                _ => println!("Format error: {}", e),
+            },
+        },
     }
 }
